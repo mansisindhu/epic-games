c0191c0abba0f58ed3ec933c88db76e687f30664
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-15 16:30:12 +0530

    Card Content Added

--- src/components/LandingPageComponents/MainCard/MainCard.jsx
+++ src/components/LandingPageComponents/MainCard/MainCard.jsx
@@ -1,6 +1,23 @@
+import styles from "./mainCard.module.css";
 const MainCard = (props) => {
-  const { image, title, tagline, price } = props;
-  return <></>;
+  const { image, title, tagline, price, discount } = props;
+  console.log(image);
+
+  return (
+    <>
+      <div className={styles.card}>
+        <img className={styles.banner} src={image} alt="cardImage" />
+        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.tagline_cont}>
+          <p className={styles.tagline}> {tagline}</p>
+        </div>
+        <div className={styles.price_div}>
+          <p>-{discount}%</p>
+          <p>₹{price}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MainCard;
--- src/components/LandingPageComponents/MainCardContainer/MainCardContainer.jsx
+++ src/components/LandingPageComponents/MainCardContainer/MainCardContainer.jsx
@@ -1,15 +1,29 @@
 import MainCard from "../MainCard/MainCard";
+import styles from "./mainCardContainer.module.css";
 
 const MainCardContainer = ({ data, heading }) => {
+  console.log(data);
+
   return (
-    <div>
-      {data.map((el) => {
-        return (
-          <>
-            <MainCard image={el.image} />
-          </>
-        );
-      })}
-    </div>
+    <>
+      <h1>{heading}</h1>
+      <div className={styles.container}>
+        {data.map((el) => {
+          return (
+            <>
+              <MainCard
+                image={el.cardImage}
+                title={el.title}
+                tagline={el.cardTagline}
+                discount={el.price.discount}
+                price={el.price.mainPrice}
+              />
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 };
+
+export default MainCardContainer;
