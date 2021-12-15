7fb031d0b4593b210b33b221491470380f13fb41
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-12-15 14:21:33 +0530

    Created signup pages

--- src/App.js
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div className="App">hello</div>;
-};
-
-export default App;
---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,5 @@
+const SignupPage = () => {
+  return <div></div>;
+};
+
+export default SignupPage;
