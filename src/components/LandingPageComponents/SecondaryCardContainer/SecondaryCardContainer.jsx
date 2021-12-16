323d265de9ac0c5675237de84c0b5784905bf0ad
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-16 08:53:07 +0530

    Added secondary card data in container

---
+++ src/components/LandingPageComponents/SecondaryCardContainer/SecondaryCardContainer.jsx
@@ -0,0 +1,30 @@
+import SecondaryCard from "../SecondaryCard/SecondaryCard";
+import styles from "./secondaryCardContainer.module.css";
+
+const SecondaryCardContainer = ({ data }) => {
+  console.log(data);
+
+  return (
+    <>
+      <div className={styles.container}>
+        {data.map((el) => {
+          return (
+            <>
+              <div className={styles.card}>
+                <SecondaryCard
+                  image={el.thumbnail}
+                  title={el.title}
+                  description={el.description}
+                  discount={el.price.discount}
+                  price={el.price.mainPrice}
+                />
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default SecondaryCardContainer;
