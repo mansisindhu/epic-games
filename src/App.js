af954033b2edd399c3b9ca7e10f2bcf736c2d196
rajnish <rajnishkumar320883@gmail.com>
2021-12-16 13:06:39 +0530

    footer changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import Header from "./components/Header"
-//import Footer from "./components/Footer"
+
 import Footer from './components/Footer/Footer'
 
 const App = () => {
   return <div className="App">
-    {/* <Header/>
    
-    <Footer/> */}
 
 <Footer/>
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,72 +1,69 @@
-
-import './footer.css';
-
-
+import styles from './Footer.module.css'
 
 export default function Footer(){
     return(
        <>
        
-           <div className="footer-top">
-               <div className="footer-icon">
-                   <img  className="img1" src="Vector4.svg"></img>
-                   <img  className="img2" src="Vector5.svg"></img>
-                   <img  className="img3" src="Vector6.svg"></img>
+           <div className={styles.footer1}>
+               <div className={styles.footer2}>
+                   <img  className={styles.img1} src="Vector4.svg"></img>
+                   <img  className={styles.img2} src="Vector5.svg"></img>
+                   <img  className={styles.img3} src="Vector6.svg"></img>
               </div>
-              <div className="footer-right">
-                   <img  className="img4" src="Vector7.svg"></img>
+              <div className={styles.footerright}>
+                   <img  className={styles.img4} src="Vector7.svg"></img>
                    </div>
-                   <div className="footer-12">
+                   <div className={styles.footer12}>
                <p>Resources</p>
            </div>
-                <div className="footer-war">
-                <div className="para">
+                <div className={styles.footerwar}>
+                <div className={styles.para}>
                     <p>Support-A-Creator</p>
                     <p>Publish on Epic Games</p>
                     <p>Careers</p>
                     </div>
-                    <div className="para">
+                    <div className={styles.para}>
                     <p>Company</p>
                     <p>Fan Art Policy</p>
                     <p>UX Research</p>
                     </div>
-                    <div className="para">
+                    <div className={styles.para}>
                     <p>Store EULA</p>
                     <p>Online Services</p>
                     <p>Community Rules</p>
                     </div>
                 </div>
 
-                <div className="footer-12">
+                <div className={styles.footer12}>
                <p>Made By Epic Games</p>
            </div>
 
-           <div className="footer-war">
-                <div className="para">
+           <div className={styles.footerwar}>
+                <div className={styles.para}>
                     <p>Battle Breakers</p>
                     <p>Fortnite</p>
                     <p>Infinity Blade</p>
                     <p>Robo Recall</p>
                     </div>
-                    <div className="para">
+                    <div className={styles.para}>
                     <p>Shadow Complex</p>
                     <p>Spyjinx</p>
                     <p>Unreal Tournament</p>
                     </div>
                      </div>
-                        <div className="line"></div>
+                        <div className={styles.line}></div>
 
-                        <div className="paragraph">
+                        <div className={styles.paragraph}>
                             <p>© 2021, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine, the Unreal Engine logo, Unreal Tournament, and the Unreal Tournament logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere. Other brands or product names are the trademarks of their respective owners. Non-US transactions through Epic Games International, S.à r.l.   </p>
                         </div>
 
-                        <div className="threeline">
+                        <div className={styles.threeline}>
                            <a>Terms of Service</a>
                            <a>Privacy Policy</a>
                            <a>Store Refund Policy</a>
                         </div>
 
-                        <div className="lastimg">
+                        <div className={styles.lastimg}>
                             <img src="shape.svg"></img>
                             <img src="path72.svg"></img>
                         </div>
