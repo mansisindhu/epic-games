f0fa90fdc8155268d7c83caddcc830728e6da247
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-17 02:05:27 +0530

    Game Card

--- src/components/GamePageComponents/GameCard/GameCard.jsx
+++ src/components/GamePageComponents/GameCard/GameCard.jsx
@@ -1,26 +1,67 @@
 import PaymentCard from "../Paymentcard";
 import styles from "./gameCard.module.css";
+import LinksCard from "../LinksCard";
+import SimpleAccordion from "../Accordion/Accordion";
+import Specifications from "../SpecificationsCard/Specifications";
+
 const GameCard = ({ data }) => {
+  console.log(data);
   return (
     <>
-      <div className={styles.gameCard}>
+      <div className={styles.main}>
         <div className={styles.container}>
-          <p>{data.title}</p>
-          <div>
-            <img src={data.thumbnail} alt="" />
-          </div>
-        </div>
+          <div className={styles.card}>
+            <p>{data.title}</p>
+            <div>
+              <img src={data.thumbnail} alt="banner" />
+            </div>
 
-        <div>
-          <PaymentCard
-            logo={data.logo}
-            discount={data.price.discount}
-            price={data.price.mainPrice}
-            developer={data.developer}
-            publisher={data.publisher}
-            releaseDate={data.releaseDate}
-            platform={data.platform}
-          />
+            <div>
+              <p>{data.description}</p>
+            </div>
+
+            <div className={styles.features_div}>
+              <div className={styles.genres}>
+                <p>Genre</p>
+                {data.genres.map((e) => (
+                  <>
+                    <p>{e}</p>
+                  </>
+                ))}
+              </div>
+
+              <div className={styles.features}>
+                <p>Features</p>
+                {data.features.map((e) => (
+                  <>
+                    <p>{e}</p>
+                  </>
+                ))}
+              </div>
+            </div>
+
+            <SimpleAccordion />
+
+            <div className={styles.links}>
+              <LinksCard />
+            </div>
+
+            <div className={styles.specifications}>
+              <Specifications />
+            </div>
+          </div>
+
+          <div className={styles.payment}>
+            <PaymentCard
+              logo={data.logo}
+              discount={data.price.discount}
+              price={data.price.mainPrice}
+              developer={data.developer}
+              publisher={data.publisher}
+              releaseDate={data.releaseDate}
+              platform={data.platform}
+            />
+          </div>
         </div>
       </div>
     </>
