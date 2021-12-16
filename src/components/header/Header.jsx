8eddfe7e3d73ca15e9d62a786544de1e1b99a96f
Abhishek M <abhimukade2000@gmail.com>
2021-12-16 21:37:26 +0530

    created header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import styles from "./styles/header.module.css";
+
+export const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    if (toggle === false) {
+      setToggle(true);
+    } else {
+      setToggle(false);
+    }
+  };
+
+  const handleClose = () => {
+    setToggle(false);
+  };
+
+  return (
+    <div className={styles.header__main__div}>
+      <div className={styles.header__first}>
+        <div className={styles.header__logo}>
+          <img src="/icons/Epic_Games_white.svg" alt="epic" />
+        </div>
+
+        <div className={styles.header__navigations}>
+          <div className={styles.header__navigations__div}>
+            <p className={styles.onhover__color__white}>STORE</p>
+          </div>
+          <div className={styles.header__navigations__div}>
+            <p className={styles.onhover__color__white}>FAQ</p>
+          </div>
+          <div className={styles.header__navigations__div}>
+            <p className={styles.onhover__color__white}>HELP</p>
+          </div>
+          <div className={styles.header__navigations__div}>
+            <p className={styles.onhover__color__white}>UNREAL ENGINE</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.header__second}>
+        <div
+          className={`${styles.padding__left} ${styles.padding__right} ${styles.display__none} ${styles.onhover__color__white}`}
+        >
+          <img src="/icons/header_global.svg" alt="global" />
+        </div>
+
+        <div
+          className={`${styles.header__second__user} ${styles.padding__left} ${styles.padding__right} ${styles.display__none}}`}
+        >
+          <div
+            className={`${styles.padding__left} ${styles.padding__right} ${styles.onhover__color__white} ${styles.display__none}`}
+          >
+            <img src="/icons/header_user_offline.svg" alt="offline user" />
+          </div>
+          <div
+            id={styles.header__rewrite}
+            className={`${styles.padding__right} ${styles.display__none}`}
+          >
+            <p className={styles.onhover__color__white}>SIGN IN</p>
+          </div>
+        </div>
+
+        <div className={`${styles.padding__left} ${styles.display__none}`}>
+          <button
+            className={`${styles.header__button} ${styles.onhover__color__blue}`}
+          >
+            DOWNLOAD
+          </button>
+        </div>
+
+        <div
+          onClick={handleToggle}
+          className={
+            toggle ? `${styles.toggle} ${styles.active}` : `${styles.toggle}`
+          }
+          style={{ backgroundColor: toggle ? "#2A2A2A" : "#0078F2" }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div
+          id="side-menu"
+          className={styles.sidemenu}
+          style={{ width: toggle ? "86%" : "" }}
+        >
+          {/* <div className={styles.closebtn' onClick={handleClose}>&times;</div> */}
+          <div className={styles.mainMenu} id="main__menu">
+            <div className={styles.sidebar__info}>
+              <p onClick={handleClose}>STORE</p>
+              <p onClick={handleClose}>FAQ</p>
+              <p onClick={handleClose}>HELP</p>
+              <p onClick={handleClose}>UNREAL ENGINE</p>
+            </div>
+
+            <div>
+              <div className={styles.slider__botton}>
+                <div className={styles.slider__botton__first}>
+                  <img
+                    src="/icons/header_user_offline.svg"
+                    alt="offline user"
+                  />
+                  <p className={styles.onhover__color__white}>SIGN IN</p>
+                </div>
+                <div className={styles.slider__botton__second}>
+                  <img src="/icons/header_global.svg" alt="global" />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  id={styles.btn__revoke}
+                  className={`${styles.header__button} ${styles.onhover__color__blue}`}
+                >
+                  DOWNLOAD
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
