3cfe7cc64ce3ee7ce469d6726eb6576869a1c913
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-12-16 12:23:54 +0530

    Added responsive filters section

--- src/pages/BrowsePage/BrowsePage.jsx
+++ src/pages/BrowsePage/BrowsePage.jsx
@@ -5,42 +5,72 @@ import PopularGenre from "../../components/BrowsePageComponents/PopularGenre";
 import styles from "./browse-page.module.css";
 import Data from "../../Data.json";
 import GameCard from "../../components/BrowsePageComponents/GameCard";
+import FilterModal from "../../components/BrowsePageComponents/Filters/FilterModal";
 
 const BrowsePage = () => {
   const [sortingModal, setSortingModalState] = useState(false);
 
-  const [filterDialog, setFilterdialog] = useState(false);
+  const [filterModal, setFilterModal] = useState(false);
 
   return (
-    <div className={styles.main}>
-      <PopularGenre />
-      <div className={styles.main_container}>
-        <div className={styles.filters_sorting_container}>
-          <div className={styles.sorting}>
-            <p className={styles.sorting_para}>
-              Sort By: <span>New release</span>
-            </p>
-          </div>
-          <div onClick={() => setFilterdialog(true)} className={styles.filters}>
-            <p>Filters</p>
-            <IoFilterSharp style={{ width: "18px", height: "18px" }} />
-          </div>
-        </div>
-        <div className={styles.data_container}>
-          {Data.map((el) => (
-            <div className={styles.card_container}>
-              <GameCard
-                image={el.cardImage}
-                title={el.title}
-                tagline={el.cardTagline}
-                price={el.price.mainPrice}
-                discount={el.price.discount}
-              />
+    <>
+      <div className={styles.main}>
+        <div className={styles.containter}>
+          <PopularGenre />
+          <div className={styles.main_container}>
+            <div className={styles.data_wrapper}>
+              <div className={styles.filters_sorting_container}>
+                <div className={styles.sorting}>
+                  <p className={styles.sorting_para}>
+                    Sort By: <span>New release</span>
+                  </p>
+                </div>
+                <div
+                  onClick={() => setFilterModal(true)}
+                  className={styles.filters}
+                >
+                  <p>Filters</p>
+                  <IoFilterSharp style={{ width: "18px", height: "18px" }} />
+                </div>
+              </div>
+              <div className={styles.data_container}>
+                {Data.map((el) => (
+                  <div className={styles.card_container}>
+                    <GameCard
+                      image={el.cardImage}
+                      title={el.title}
+                      tagline={el.cardTagline}
+                      price={el.price.mainPrice}
+                      discount={el.price.discount}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className={styles.filters_desktop}>
+                <FilterModal />
+              </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
-    </div>
+
+      {filterModal ? (
+        <div className={styles.modal_overlay}>
+          <div className={styles.modal_container}>
+            <FilterModal handleClose={setFilterModal} />
+          </div>
+          <div className={styles.bottom_navbar}>
+            <div className={styles.clear}>CLEAR</div>
+            <button
+              onClick={() => setFilterModal(false)}
+              className={styles.done}
+            >
+              DONE
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
