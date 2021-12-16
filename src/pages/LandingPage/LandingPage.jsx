76634c0fb52451d1ae659d6b1c08cfd53d8e8b72
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-16 08:54:36 +0530

    Landing page section added

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,40 @@
+import BrowseLink from "../../components/LandingPageComponents/BrowseLink/BrowseLink";
 import MainCardContainer from "../../components/LandingPageComponents/MainCardContainer/MainCardContainer";
 import Data from "../../Data.json";
+import CardData from "../../CardData.json";
+import SecondaryCardContainer from "../../components/LandingPageComponents/SecondaryCardContainer";
+
+import styles from "./landingPage.module.css";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const LandingPage = () => {
   const sale = Data;
+  const SecondaryCardData = CardData;
 
   return (
     <>
-      <h1>Games on Sale </h1>
-      <MainCardContainer data={sale} />
+      <div className={styles.landing_container}>
+        <div className={styles.heading_div}>
+          <h4 className={styles.heading}>Games on Sale</h4>
+          <div className={styles.icon}>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+        </div>
+
+        <div className={styles.mainCardContainer}>
+          <MainCardContainer data={sale} />
+        </div>
+        <SecondaryCardContainer
+          data={[SecondaryCardData[0], SecondaryCardData[1]]}
+        />
+        <SecondaryCardContainer
+          data={[SecondaryCardData[2], SecondaryCardData[3]]}
+        />
+        <SecondaryCardContainer
+          data={[SecondaryCardData[4], SecondaryCardData[5]]}
+        />
+        <BrowseLink />
+      </div>
     </>
   );
 };
