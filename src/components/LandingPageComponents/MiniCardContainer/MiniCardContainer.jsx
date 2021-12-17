55ae5feea7b5f19e8a445a0171e828a250a830f4
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-18 01:01:45 +0530

    Responsive and styled

--- src/components/LandingPageComponents/MiniCardContainer/MiniCardContainer.jsx
+++ src/components/LandingPageComponents/MiniCardContainer/MiniCardContainer.jsx
@@ -5,19 +5,21 @@ const MiniCardContainer = ({ data, heading }) => {
   return (
     <>
       <div className={styles.main}>
-        <p>{heading}</p>
+        <div className={styles.container}>
+          <p className={styles.cont_heading}>{heading}</p>
 
-        {data.map((el, i) => {
-          return (
-            <div key={i} className={styles.card}>
-              <MiniCard
-                banner={el.cardImage}
-                title={el.title}
-                price={el.price}
-              />
-            </div>
-          );
-        })}
+          {data.map((el, i) => {
+            return (
+              <div key={i} className={styles.card}>
+                <MiniCard
+                  banner={el.cardImage}
+                  title={el.title}
+                  price={el.price}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
