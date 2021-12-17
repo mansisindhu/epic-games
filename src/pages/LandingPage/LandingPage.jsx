ff02f0e6ea6b89891e7bf3a7d78d6931bc4f9af3
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-18 01:57:20 +0530

    Landing page complete

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -11,6 +11,9 @@ import MiniCardContainer from "../../components/LandingPageComponents/MiniCardCo
 
 import BrowseLink from "../../components/LandingPageComponents/BrowseLink/BrowseLink";
 
+import Header from "../../components/header";
+import SubNavbar from "../../components/SubNavbar";
+
 const LandingPage = () => {
   const sale = Data;
   const secondaryCardData = Data;
@@ -18,50 +21,54 @@ const LandingPage = () => {
 
   return (
     <>
+      <Header />
       <div className={styles.main}>
         <div className={styles.landing_container}>
-          <div className={styles.mainCardContainer}>
-            <div className={styles.data_cont}>
-              <MainCardContainer data={sale} />
-            </div>
-          </div>
-
-          <div className={styles.secondaryCardContainer}>
-            <SecondaryCardContainer
-              data={[secondaryCardData[15], secondaryCardData[12]]}
-            />
-          </div>
-
-          <div className={styles.minicard}>
-            <div className={styles.miniCardContainer}>
-              <MiniCardContainer data={miniCardData} heading="New Releases" />
+          <SubNavbar />
+          <div className={styles.main_container}>
+            <div className={styles.mainCardContainer}>
+              <div className={styles.data_cont}>
+                <MainCardContainer data={sale} />
+              </div>
             </div>
 
-            <div className={styles.miniCardContainer}>
-              <MiniCardContainer data={miniCardData} heading="Top Sellers" />
+            <div className={styles.secondaryCardContainer}>
+              <SecondaryCardContainer
+                data={[secondaryCardData[6], secondaryCardData[14]]}
+              />
             </div>
 
-            <div className={styles.miniCardContainer}>
-              <MiniCardContainer data={miniCardData} heading="Coming Soon" />
+            <div className={styles.freeCardContainer}>
+              <FreeCardContainer />
             </div>
-          </div>
 
-          <div className={styles.freeCardContainer}>
-            <FreeCardContainer />
-          </div>
+            <div className={styles.minicard}>
+              <div className={styles.miniCardContainer}>
+                <MiniCardContainer data={miniCardData} heading="New Releases" />
+              </div>
 
-          <div className={styles.secondaryCardContainer}>
-            <SecondaryCardContainer
-              data={[secondaryCardData[2], secondaryCardData[3]]}
-            />
-          </div>
+              <div className={styles.miniCardContainer}>
+                <MiniCardContainer data={miniCardData} heading="Top Sellers" />
+              </div>
 
-          <div className={styles.secondaryCardContainer}>
-            <SecondaryCardContainer
-              data={[secondaryCardData[4], secondaryCardData[5]]}
-            />
+              <div className={styles.miniCardContainer}>
+                <MiniCardContainer data={miniCardData} heading="Coming Soon" />
+              </div>
+            </div>
+
+            <div className={styles.secondaryCardContainer}>
+              <SecondaryCardContainer
+                data={[secondaryCardData[2], secondaryCardData[3]]}
+              />
+            </div>
+
+            <div className={styles.secondaryCardContainer}>
+              <SecondaryCardContainer
+                data={[secondaryCardData[4], secondaryCardData[5]]}
+              />
+            </div>
+            <BrowseLink />
           </div>
-          <BrowseLink />
         </div>
       </div>
     </>
