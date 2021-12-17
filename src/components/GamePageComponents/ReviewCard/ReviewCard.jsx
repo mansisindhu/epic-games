0b079cc5c240f79a383387461ade13593f17fe65
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-17 15:56:13 +0530

    Review card styling

--- src/components/GamePageComponents/ReviewCard/ReviewCard.jsx
+++ src/components/GamePageComponents/ReviewCard/ReviewCard.jsx
@@ -1,18 +1,22 @@
+import styles from "./reviewcard.module.css";
+
 const ReviewCard = (props) => {
   const { author, organisation, rating, description, link } = props;
 
   return (
     <>
-      <div>
-        <div>
+      <div className={styles.main}>
+        <div className={styles.name_div}>
           <p>{organisation}</p>
-          <p>by {author}</p>
+          <p className={styles.author}>by {author}</p>
         </div>
 
-        <div>
-          <p> {rating}</p>
-          <p>{description}</p>
-          <a href={link}></a>
+        <div className={styles.content}>
+          <p>{rating} / 10</p>
+          <p className={styles.description}>{description}</p>
+          <div className={styles.reviewlink}>
+            <a href={link}>Read All Reviews</a>
+          </div>
         </div>
       </div>
     </>
