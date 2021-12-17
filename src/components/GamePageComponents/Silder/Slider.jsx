749c237693176609504895a7562a9a7c49a5f295
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-12-17 13:08:26 +0530

    Created slider component for game page

---
+++ src/components/GamePageComponents/Silder/Slider.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import "./slider.css";
+
+const Slider = ({ data }) => {
+  return (
+    <Carousel autoPlay showStatus={false}>
+      {data.heroImages.map((el, i) => {
+        return (
+          <div>
+            <img alt={`slider${i + 1}`} src={el} />
+          </div>
+        );
+      })}
+    </Carousel>
+  );
+};
+
+export default Slider;
