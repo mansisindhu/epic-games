e0e02ff8fb1dda8683e803d2bed45c847dfcbd6f
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-18 21:34:43 +0530

    Payment Page folder struture

---
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./payment.module.css";
+
+const PaymentPage = () => {
+  return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.containter}></div>
+      </div>
+    </>
+  );
+};
+
+export default PaymentPage;
