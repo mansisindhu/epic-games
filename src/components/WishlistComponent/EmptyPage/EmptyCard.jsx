ae084da0286e5c02152084d66bde7813b6b839ff
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-18 19:59:30 +0530

    Empty page styles and responsive for wishlist page

---
+++ src/components/WishlistComponent/EmptyPage/EmptyCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./emptyCard.module.css";
+
+const EmptyCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.image_div}>
+        <img src="./icons/sorry.svg" alt="noResults" />
+      </div>
+      <p className={styles.heading}>
+        You haven't added anything to your wishlist yet.
+      </p>
+
+      <Link className={styles.link}>Shop for Apps & Games</Link>
+    </div>
+  );
+};
+
+export default EmptyCard;
