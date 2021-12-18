ab9a4f3227f5de1fe3f8df06002ca8551f137863
Abhishek M <abhimukade2000@gmail.com>
2021-12-19 04:51:38 +0530

    created responsive payment page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Header from "./components/header/Header";
 import PaymentPage from "./pages/PaymentPage";
 import WishlistPage from "./pages/WishlistPage";
 import { Main } from './components/Carousel/Main'
+import { MobilePaymentModal } from "./components/PaymentPageComponents/MobilePaymentModal";
 
 
 const App = () => {
   return <div className="App">
     <PaymentPage />
+    {/* <MobilePaymentModal /> */}
     <Header />
     <Main />
     <WishlistPage />
