452e08858bbf7a33358c009a37a3f5c3ca143417
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-18 21:44:00 +0530

    Sidebar for game details for payment page created

---
+++ src/components/PaymentPageComponents/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import styles from "./sidebar.module.css";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className={styles.container}>hello</div>
+    </>
+  );
+};
+
+export default Sidebar;
