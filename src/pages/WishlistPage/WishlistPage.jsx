459d7f6494cc8c5dc805ec693b8a38f104162516
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-12-19 00:20:18 +0530

    Added wishlist data on wishlist page

--- src/pages/WishlistPage/WishlistPage.jsx
+++ src/pages/WishlistPage/WishlistPage.jsx
@@ -5,23 +5,47 @@ import SubNavbar from "../../components/SubNavbar";
 import NotificationBar from "../../components/WishlistComponent/NotificationBar/NotificationBar";
 import EmptyCard from "../../components/WishlistComponent/EmptyPage/EmptyCard";
 import Card from "../../components/WishlistComponent/Card/Card";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const WishListPage = () => {
+  const user = useSelector((state) => state.user);
+  const data = useSelector((state) => state.landingPageData);
+  const wishlist = data.filter((el) => {
+    if (user.wishlist?.includes(el._id)) {
+      return el;
+    }
+  });
+
   return (
     <>
       <Header />
       <div className={styles.main}>
         <div className={styles.containter}>
-          <SubNavbar />
+          <div className={styles.sub_navbar}>
+            <SubNavbar />
+          </div>
 
           <div className={styles.main_container}>
             <p>Wishlist </p>
-
             <NotificationBar />
 
-            {/* <EmptyCard /> */}
-            <Card />
-            <Card />
+            {wishlist.length ? (
+              wishlist.map((el) => {
+                return (
+                  <Link to={`/games/${el._id}`}>
+                    <Card
+                      id={el._id}
+                      title={el.title}
+                      price={el.price}
+                      cardImage={el.cardImage}
+                    />
+                  </Link>
+                );
+              })
+            ) : (
+              <EmptyCard />
+            )}
           </div>
         </div>
       </div>
