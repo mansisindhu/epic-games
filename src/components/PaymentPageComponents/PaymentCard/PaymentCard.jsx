930b2fce74e2dae8038939df598799e21a6c07d5
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-12-19 01:35:57 +0530

    Added radios cards

--- src/components/PaymentPageComponents/PaymentCard/PaymentCard.jsx
+++ src/components/PaymentPageComponents/PaymentCard/PaymentCard.jsx
@@ -1,53 +1,34 @@
-import * as React from "react";
-import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Fade from "@mui/material/Fade";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import React from "react";
+import styles from "./payment-card.module.css";
+import { IoMdCard } from "react-icons/io";
+import { SiPaypal } from "react-icons/si";
 
 const PaymentCard = () => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-          </Box>
-        </Fade>
-      </Modal>
+      <div className={styles.payment_method}>
+        <input
+          className={styles.radio}
+          type="radio"
+          name="paymentMethod"
+        ></input>
+        <div className={styles.icon}>
+          <IoMdCard />
+        </div>
+        <label for="payment">Credit Card</label>
+      </div>
+
+      <div className={styles.payment_method}>
+        <input
+          className={styles.radio}
+          type="radio"
+          name="paymentMethod"
+        ></input>
+        <div className={styles.icon}>
+          <SiPaypal />
+        </div>
+        <label for="payment">Paypal</label>
+      </div>
     </div>
   );
 };
